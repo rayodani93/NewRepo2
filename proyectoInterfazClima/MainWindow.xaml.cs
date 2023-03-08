50644634b8579d53dde7012ebe7b2f7d51b46ea1
Administrador <rayodani93@gmail.com>
2023-03-08 18:22:34 +0100

    proyecto finalizado

---
+++ proyectoInterfazClima/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace proyectoInterfazClima
+{
+    
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+            
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void botonCalcular(object sender, RoutedEventArgs e)
+        {
+            //al presionar en el boton CalcularMedia se calcula la media del alumno
+            
+
+        }
+
+        private void botonSalir(object sender, RoutedEventArgs e)
+        {
+          
+        }
+    }
+}
