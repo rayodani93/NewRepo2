ec230651a72eb0333b58a51b1b936e78eb3cdcff
Administrador <rayodani93@gmail.com>
2023-03-10 16:21:35 +0100

    acabado

--- proyectoInterfazClima/MainWindow.xaml.cs
+++ proyectoInterfazClima/MainWindow.xaml.cs
@@ -15,25 +15,25 @@ using System.Windows.Shapes;
 
 namespace proyectoInterfazClima
 {
-    
+
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
-           
-            
+
+
         {
-            
+
             InitializeComponent();
             //string alumno = nombre.Text;
             ventana1.ControlBox = false;
-            
-            
-            
-            
-            
+
+
+
+
+
         }
         public string alumnoAprobado { get; set; }
 
@@ -43,52 +43,54 @@ namespace proyectoInterfazClima
 
 
         public bool ControlBox { get; private set; }
-        
+
 
         private void botonCalcular(object sender, RoutedEventArgs e)
         {
-            //Window1 ventanaAlumno = new Window1();
+            double primerExamen, segundoExamen, trabajoClase, asistencia;
 
-       
+            if (!double.TryParse(PrimerExamen.Text, out primerExamen) || primerExamen < 0 || primerExamen > 10 ||
+                !double.TryParse(SegundoExamen.Text, out segundoExamen) || segundoExamen < 0 || segundoExamen > 10 ||
+                !double.TryParse(TrabajoClase.Text, out trabajoClase) || trabajoClase < 0 || trabajoClase > 10 ||
+                !double.TryParse(Asistencia.Text, out asistencia) || asistencia < 0 || asistencia > 10)
+            {
+                MessageBox.Show("Solo se permiten valores numéricos entre 0 y 10 en los cuadros de notas",
+                                "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                // Vaciar el textbox que tiene un valor no numérico
+                if (!double.TryParse(PrimerExamen.Text, out primerExamen) || primerExamen < 0 || primerExamen > 10)
+                    PrimerExamen.Text = "";
+                if (!double.TryParse(SegundoExamen.Text, out segundoExamen) || segundoExamen < 0 || segundoExamen > 10)
+                    SegundoExamen.Text = "";
+                if (!double.TryParse(TrabajoClase.Text, out trabajoClase) || trabajoClase < 0 || trabajoClase > 10)
+                    TrabajoClase.Text = "";
+                if (!double.TryParse(Asistencia.Text, out asistencia) || asistencia < 0 || asistencia > 10)
+                    Asistencia.Text = "";
 
-        double primerExamen = double.Parse(PrimerExamen.Text);
-            double segundoExamen = double.Parse(SegundoExamen.Text);
-            double trabajoClase = double.Parse(TrabajoClase.Text);
-            double asistencia = double.Parse(Asistencia.Text);
+                return;
+            }
 
             media = (primerExamen * 0.35) + (trabajoClase * 0.2) + (asistencia * 0.1) + (segundoExamen * 0.35);
 
-            //double media = (primerExamen * 0.35) + (trabajoClase * 0.2) + (asistencia * 0.1) + (segundoExamen * 0.35);
-            if (string.IsNullOrEmpty(PrimerExamen.Text) || string.IsNullOrEmpty(SegundoExamen.Text) || string.IsNullOrEmpty(TrabajoClase.Text)
-                || string.IsNullOrEmpty(Asistencia.Text) || string.IsNullOrEmpty(nombre.Text))
+            if (string.IsNullOrEmpty(nombre.Text))
             {
-                
                 MessageBox.Show("Debes rellenar todos los cuadros de texto para poder continuar", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (media >= 5)
+            {
+                MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
+                mainWindow.alumnoAprobado = nombre.Text;
+                Window2 ventanaAprobado = new Window2();
+                ventanaAprobado.Show();
             }
             else
             {
-                if (media >= 5)
-                {
-                    MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
-                    mainWindow.alumnoAprobado = nombre.Text;
-                    Window2 ventanaAprobado = new Window2();
-                    ventanaAprobado.Show();
-
-
-                }
-                else
-                {
-                    MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
-                    mainWindow.alumnoSuspenso = nombre.Text;
-                    Window1 ventanaSuspenso = new Window1();
-                    ventanaSuspenso.Show();
-                }
+                MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
+                mainWindow.alumnoSuspenso = nombre.Text;
+                Window1 ventanaSuspenso = new Window1();
+                ventanaSuspenso.Show();
             }
-            
-            
-            
-
-            
         }
 
         private void botonSalir(object sender, RoutedEventArgs e)
